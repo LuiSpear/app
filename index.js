e618b1ca96557fd8c5b4bbb8d4050676462964c8
LuiSpear <lcorona11@ucol.mx>
2022-11-04 18:55:29 -0600

    version5

---
+++ index.js
@@ -0,0 +1,17 @@
+let variable = "Hola mundo";
+alert (variable);
+console.log(variable);
+
+function saludo(){
+let nombre = "Luis";
+let variable = "Hi " +nombre;
+alert(variable);
+}
+ function suma(dato1,dato2){
+    let resultado=dato1+dato2;
+    alert ("El resultado es "+resultado);
+
+ }
+ prompt("Esto es un prompt");
+ suma(20,1);
+
