1f1364bc762ea65793f64a5397d378a3f5c27ea4
LuiSpear <lcorona11@ucol.mx>
2022-11-16 18:41:28 -0600

    version8

--- Ejercicio2/inicio.js
+++ Ejercicio2/inicio.js
@@ -1,22 +1,51 @@
+
+let contador = 0;
+ while(contador == 10) {
+     document.write(contador + "<br>");
+     contador++;
+ }
+
+do {
+    document.write(contador + "<br>");
+    contador++;
+} while (contador == 10);
+
+
+
+
 function numeroMayor() {
     let numero1= parseInt(document.getElementById("numero1").value);
     let numero2= parseInt(document.getElementById("numero2").value);
     console.log(numero1);
     console.log(numero2);
-    
-while (numero != " "  && numero2 !=" ") {
-    console.log("Estan vacios");
 }
+    
+    
 
-if (numero1>=numero2) { 
-    if (numero1 == numero2) {
-        alert("los numeros son iguales");
-            } else {
-                alert("El numero mayor es: "+numero1);
-            }
-}else{
-    alert("El numero mayor es: "+numero2);
-    }
-}
-            
-    
+
+    // while (!isNaN(numero1) && !isNaN(numero2)) {
+//     console.log("Se hace la comparacion de los numeros");
+//     if (numero1>=numero2) { 
+//         if (numero1 == numero2) {
+//             alert("los numeros son iguales");
+//         } else {
+//             alert("El numero mayor es: "+numero1);
+//                }
+//         }else{
+//              alert("El numero mayor es: "+numero2);
+//             }
+//     break;
+//  }
+// }
+
+// if (numero1>=numero2) { 
+//     if (numero1 == numero2) {
+//         alert("los numeros son iguales");
+//             } else {
+//                 alert("El numero mayor es: "+numero1);
+//             }
+// }else{
+//     alert("El numero mayor es: "+numero2);
+//     }
+// }
+
